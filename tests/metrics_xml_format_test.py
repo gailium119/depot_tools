b52eb7428f9753a6bab6f7f5fc3e958c6ef21091
Scott Lee <ddoman@chromium.org>
2024-12-05 05:31:40 +0000

    add metrics_xml_formatter
    git cl format supports formatting certain xml files under tools/metrics.
    This CL adds metrics_xml_formatter so that the formatter can be run
    with git cl format.
    
    Bug: b/369827156
    Change-Id: I5922cd79304aa8e06917aacc9f2d9bd3ed548c2f

---
+++ tests/metrics_xml_format_test.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env vpython3
+# coding=utf-8
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import sys
+import unittest
+from unittest import mock
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import metrics_xml_format
+
+norm = lambda path: path.replace('/', os.sep)
+findTool = lambda path: metrics_xml_format.FindMetricsXMLFormatterTool(
+    norm(path), verbose=True)
+
+
+class GetMetricsDirTest(unittest.TestCase):
+
+    def testWithAbsolutePath(self):
+        get = lambda path: metrics_xml_format.GetMetricsDir(norm(path))
+        self.assertTrue(get('/src/tools/metrics/actions/abc.xml'))
+        self.assertTrue(get('/src/tools/metrics/histograms/abc.xml'))
+        self.assertTrue(get('/src/tools/metrics/structured/abc.xml'))
+        self.assertTrue(get('/src/tools/metrics/ukm/abc.xml'))
+
+        self.assertFalse(get('/src/tools/metrics/actions/next/abc.xml'))
+        self.assertFalse(get('/src/tools/metrics/histograms/next/abc.xml'))
+        self.assertFalse(get('/src/tools/metrics/structured/next/abc.xml'))
+        self.assertFalse(get('/src/tools/metrics/ukm/next/abc.xml'))
+
+    def testWithRelativePaths(self):
+        get = lambda path: metrics_xml_format.GetMetricsDir(norm(path))
+        cwd = os.getcwd()
+        with mock.patch('os.getcwd') as getcwd:
+            getcwd.return_value = os.path.join(cwd, 'tools')
+            self.assertFalse(get('abc.xml'))
+            self.assertTrue(get('metrics/actions/abc.xml'))
+
+
+'''class FindMetricsXMLFormatTool(unittest.TestCase):
+
+    @mock.patch('gclient_paths.GetPrimarySolutionPath',
+                return_value=norm('/src' if))
+         = 'C:\\' if sys.platform == 'win32' else '/'
+    @mock.patch('os.getcwd', return_value=norm('/src/'))
+    def testWithMetricsXML(self, cwd, solution_path):
+        self.assertEqual(
+            findTool('tools/metrics/actions/abc.xml'),
+            norm('/src/tools/metrics/actions/pretty_print.py'),
+        )
+        self.assertEqual(
+            findTool('/src/tools/metrics/actions/abc.xml'),
+            norm('/src/tools/metrics/actions/pretty_print.py'),
+        )
+
+    @mock.patch('gclient_paths.GetPrimarySolutionPath',
+                return_value=norm('/src/'))
+    def testWthNonMetricsXML(self, solution_path):
+        self.assertEqual(
+            findTool('tools/metrics/actions/next/abc.xml'),
+            '',
+        )
+
+    @mock.patch('gclient_paths.GetPrimarySolutionPath', return_value=None)
+    def testWithNonCheckout(self, solution_path):
+        self.assertEqual(
+            findTool('tools/metrics/actions/abc.xml'),
+            '',
+        )
+
+    @mock.patch('gclient_paths.GetPrimarySolutionPath',
+                return_value=norm('/src/'))
+    @mock.patch('os.getcwd', return_value=norm('/src/'))
+    def testWithDifferentCheckout(self, cwd, solution_path):
+        self.assertEqual(
+            # this is the case the tool was given a file path that is located
+            # in a different checkout folder.
+            findTool('/different/src/tools/metrics/actions/abc.xml'),
+            '',
+        )
+'''
+
+
+if __name__ == '__main__':
+    unittest.main()

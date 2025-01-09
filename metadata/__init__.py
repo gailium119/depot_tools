4c12f1a6f3e561f3dbf7196f690351319eddc69f
Micah Bales <micahbales@google.com>
2025-01-09 11:21:10 -0800

    silly change
    Change-Id: I0df3d15f8b469f1f11a703e760b1af0bdf962367

--- metadata/__init__.py
+++ metadata/__init__.py
@@ -1,4 +1,5 @@
 # Copyright 2023 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+# just a little code change for a test
 """File to enable importing from this folder."""

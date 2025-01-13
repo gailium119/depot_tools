ccc863c93f34c9570d2a14c2fe57068fd8ba498a
Jordan Brown <rop@google.com>
2025-01-12 20:38:54 -0800

    Sort entries by name in allowlist files.
    This makes the allowlist files easier to read. It also prevents accidental changes when adding new entries.
    
    Note that this CL does not modify the actual entries. A duplicate of ISC was removed.
    
    Change-Id: Id64e64dbc934af1b412fce0e0c7c34a8d7b4c5f8

--- metadata/fields/custom/license_allowlist.py
+++ metadata/fields/custom/license_allowlist.py
@@ -10,43 +10,50 @@
 # Licenses below are grouped by restrictiveness level and then alphabetically.
 ALLOWED_SPDX_LICENSES = frozenset([
     # other_ignorable.
+    # go/keep-sorted start case=no
     "LZMA-SDK-9.22",
+    # go/keep-sorted end
     # unencumbered.
+    # go/keep-sorted start case=no
+    "blessing",
     "CC0-1.0",
     "Unlicense",
-    "blessing",
+    # go/keep-sorted end
     # permissive.
+    # go/keep-sorted start case=no
     "GPL-2.0-with-autoconf-exception",
     "GPL-2.0-with-classpath-exception",
     "GPL-3.0-with-autoconf-exception",
     "MIT-0",
+    # go/keep-sorted end
     # notice.
+    # go/keep-sorted start case=no
     "AML",
     "Apache-2.0",
     "Artistic-2.0",
-    "BSD-2-Clause",
+    "Beerware",
     "BSD-2-Clause-FreeBSD",
-    "BSD-3-Clause",
+    "BSD-2-Clause",
     "BSD-3-Clause-Attribution",
-    "BSD-4-Clause",
+    "BSD-3-Clause",
     "BSD-4-Clause-UC",
+    "BSD-4-Clause",
     "BSD-4.3TAHOE",
     "BSD-Source-Code",
     "BSL-1.0",
-    "Beerware",
     "CC-BY-3.0",
     "CC-BY-4.0",
     "FTL",
-    "HPND",
     "HPND-sell-variant",
+    "HPND",
     "ICU",
     "IJG",
     "ISC",
     "JSON",
     "Libpng",
-    "ISC",
-    "MIT",
+    "libtiff",
     "MIT-Modern-Variant",
+    "MIT",
     "MS-PL",
     "NAIST-2003",
     "NCSA",
@@ -59,56 +66,63 @@ ALLOWED_SPDX_LICENSES = frozenset([
     "Unicode-DFS-2016",
     "X11",
     "Zlib",
-    "libtiff",
+    # go/keep-sorted end
 ])
 
 # These are licenses that are not in the SPDX license list, but are identified
 # by the license classifier.
 EXTENDED_LICENSE_CLASSIFIERS = frozenset([
     # unencumbered.
+    # go/keep-sorted start case=no
     "AhemFont",
     "Android-SDK",
     "LZMA",
-    "SPL-SQRT-FLOOR",
     "public-domain-md5",
+    "SPL-SQRT-FLOOR",
+    # go/keep-sorted end
     # permissive.
+    # go/keep-sorted start case=no
     "LicenseRef-AMSFonts-2.2",
     "test_fonts",
+    # go/keep-sorted end
     # notice.
+    # go/keep-sorted start case=no
     "Apache-with-LLVM-Exception",
     "Apache-with-Runtime-Exception",
+    "Bitstream",
     "BSD-2-Clause-Flex",
     "BSD-3-Clause-OpenMPI",
     "BSD-4-Clause-Wasabi",
-    "Bitstream",
-    "CERN",
     "Caffe",
+    "CERN",
+    "cURL",
+    "dso",
     "Entenssa",
     "FFT2D",
+    "getopt",
     "GIF-Encoder",
     "GNU-All-permissive-Copying-License",
     "IBM-DHCP",
     "JsonCPP",
     "Khronos",
     "Libpng-2.0",
-    "LicenseRef-OpenGLUT",
     "LicenseRef-base64",
+    "LicenseRef-OpenGLUT",
     "LicenseRef-takuya-ooura",
+    "pffft",
     "Punycode",
     "SSLeay",
     "WebM-Project-Patent",
     "X11-Lucent",
-    "cURL",
-    "dso",
-    "getopt",
-    "pffft",
     "zxing",
+    # go/keep-sorted end
 ])
 
 # These licenses are only allowed in open source projects due to their
 # reciprocal requirements.
 OPEN_SOURCE_SPDX_LICENSES = frozenset([
     # reciprocal.
+    # go/keep-sorted start case=no
     "APSL-2.0",
     "CDDL-1.0",
     "CDDL-1.1",
@@ -116,6 +130,30 @@ OPEN_SOURCE_SPDX_LICENSES = frozenset([
     "EPL-1.0",
     "MPL-1.1",
     "MPL-2.0",
+    # go/keep-sorted end
+])
+
+# TODO(b/388620886): Implement warning when changing _to_ these licenses
+# (but not every time the README.chromium file is modified).
+WITH_PERMISSION_ONLY = frozenset([
+    # restricted.
+    # go/keep-sorted start case=no
+    "CC-BY-SA-3.0",
+    "GPL-2.0",
+    "GPL-3.0",
+    "LGPL-2.0",
+    "LGPL-2.1",
+    "LGPL-3.0",
+    "NPL-1.1",
+    # go/keep-sorted end
+    # by_exception_only.
+    # go/keep-sorted start case=no
+    "Commercial",
+    "LicenseRef-Play-Core-SDK-TOS",
+    "LicenseRef-Unity-Companion-License-1.3",
+    "Opus-Patent-BSD-3-Clause",
+    "UnRAR",
+    # go/keep-sorted end
 ])
 
 ALLOWED_LICENSES = ALLOWED_SPDX_LICENSES | EXTENDED_LICENSE_CLASSIFIERS

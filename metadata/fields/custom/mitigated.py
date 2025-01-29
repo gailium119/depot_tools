693f949ecb613aef005b947ab5d329027f13beab
Jordan Brown <rop@google.com>
2025-01-29 00:28:10 -0800

    [dependency_metadata] Allow descriptions for CVEs
    This adds a new way to report CVEs that includes an accompanying
    description. It also adds a new validation check that ensures that the
    CVE description is present for every entry listed in the 'Mitigated:'
    field.
    
    The 'mitigated' custom field library now exports the vulnerability regex
    in two flavours: with anchors, and without. This is so that the parser
    can extract them into self._entries, and then later in
    dependency_metadata, these values can be extracted for aggregation and
    validation.
    
    ## New Warnings added Warnings will now be created if cves (or other
    vuln ids) are not uniformly provided in both the "Mitigated" list, as
    well as in their own entries.
    
    Bug: b/392026683
    Change-Id: Ie55595970b49d705ac532f1f8c41ff47d959f56c

--- metadata/fields/custom/mitigated.py
+++ metadata/fields/custom/mitigated.py
@@ -20,8 +20,8 @@ _VULN_PREFIXES = [
 ]
 
 _PREFIX_PATTERN = "|".join(_VULN_PREFIXES)
-_VULN_ID_PATTERN = re.compile(rf"^({_PREFIX_PATTERN})-[a-zA-Z0-9]{{4}}-[a-zA-Z0-9:-]+$")
-
+VULN_ID_PATTERN = re.compile(rf"({_PREFIX_PATTERN})-[a-zA-Z0-9]{{4}}-[a-zA-Z0-9:-]+")
+VULN_ID_PATTERN_WITH_ANCHORS = re.compile(f"^{VULN_ID_PATTERN.pattern}$")
 def validate_cves(cves: str) -> Tuple[List[str], List[str]]:
     """
     Validates a list of vulnerability identifiers and returns valid and invalid IDs.
@@ -44,7 +44,7 @@ def validate_cves(cves: str) -> Tuple[List[str], List[str]]:
 
     for cve in cves.split(","):
         cve_stripped = cve.strip()
-        if bool(_VULN_ID_PATTERN.match(cve_stripped)):
+        if bool(VULN_ID_PATTERN_WITH_ANCHORS.match(cve_stripped)):
             valid_cves.append(cve_stripped)
         else:
             invalid_cves.append(cve)

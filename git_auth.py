3be37602825f625b350cfe8306fd9058e8302d80
Allen Li <ayatane@chromium.org>
2025-03-11 22:15:25 +0000

    [git_auth] Use pathless URL for cred helper
    The credential helper rules should not have a path, while the URL
    rewrite rules should.
    
    Bug: b/401338175
    Change-Id: I79b52ab4af62367363617b2a9afa03a67f5ea0b9

--- git_auth.py
+++ git_auth.py
@@ -36,7 +36,7 @@ class ConfigChanger(object):
     #
     # Increment this when making changes to the config, so that reliant
     # code can determine whether the config needs to be re-applied.
-    VERSION: int = 4
+    VERSION: int = 5
 
     def __init__(
         self,
@@ -67,12 +67,20 @@ class ConfigChanger(object):
         return _url_shortname(parts)
 
     @functools.cached_property
-    def _base_url(self) -> str:
+    def _host_url(self) -> str:
+        # Example: https://chromium.googlesource.com
+        # Example: https://chromium-review.googlesource.com
+        parts: urllib.parse.SplitResult = urllib.parse.urlsplit(
+            self._remote_url)
+        return _url_host_url(parts)
+
+    @functools.cached_property
+    def _root_url(self) -> str:
         # Example: https://chromium.googlesource.com/
         # Example: https://chromium-review.googlesource.com/
         parts: urllib.parse.SplitResult = urllib.parse.urlsplit(
             self._remote_url)
-        return parts._replace(path='/', query='', fragment='').geturl()
+        return _url_root_url(parts)
 
     @classmethod
     def new_from_env(cls, cwd: str, cl: git_cl.Changelist) -> ConfigChanger:
@@ -150,7 +158,7 @@ class ConfigChanger(object):
 
     def _apply_cred_helper(self, cwd: str) -> None:
         """Apply config changes relating to credential helper."""
-        cred_key: str = f'credential.{self._base_url}.helper'
+        cred_key: str = f'credential.{self._host_url}.helper'
         if self.mode == ConfigMode.NEW_AUTH:
             self._set_config(cwd, cred_key, '', modify_all=True)
             self._set_config(cwd, cred_key, 'luci', append=True)
@@ -172,7 +180,7 @@ class ConfigChanger(object):
             self._set_config(cwd, http_key, self._remote_url, modify_all=True)
         elif self.mode == ConfigMode.NEW_AUTH_SSO:
             self._set_config(cwd, 'protocol.sso.allow', 'always')
-            self._set_config(cwd, sso_key, self._base_url, modify_all=True)
+            self._set_config(cwd, sso_key, self._root_url, modify_all=True)
             self._set_config(cwd, http_key, None, modify_all=True)
         elif self.mode == ConfigMode.NO_AUTH:
             self._set_config(cwd, 'protocol.sso.allow', None)
@@ -196,7 +204,7 @@ class ConfigChanger(object):
 
     def _apply_global_cred_helper(self, cwd: str) -> None:
         """Apply config changes relating to credential helper."""
-        cred_key: str = f'credential.{self._base_url}.helper'
+        cred_key: str = f'credential.{self._host_url}.helper'
         if self.mode == ConfigMode.NEW_AUTH:
             self._set_config(cwd, cred_key, '', scope='global', modify_all=True)
             self._set_config(cwd, cred_key, 'luci', scope='global', append=True)
@@ -229,7 +237,7 @@ class ConfigChanger(object):
                              scope='global')
             self._set_config(cwd,
                              sso_key,
-                             self._base_url,
+                             self._root_url,
                              scope='global',
                              modify_all=True)
         elif self.mode == ConfigMode.NO_AUTH:
@@ -323,3 +331,21 @@ def _url_shortname(parts: urllib.parse.SplitResult) -> str:
     if name.endswith('-review'):
         name = name[:-len('-review')]
     return name
+
+
+def _url_host_url(parts: urllib.parse.SplitResult) -> str:
+    """Format URL with host only (no path).
+
+    Example: https://chromium.googlesource.com
+    Example: https://chromium-review.googlesource.com
+    """
+    return parts._replace(path='', query='', fragment='').geturl()
+
+
+def _url_root_url(parts: urllib.parse.SplitResult) -> str:
+    """Format URL with root path.
+
+    Example: https://chromium.googlesource.com/
+    Example: https://chromium-review.googlesource.com/
+    """
+    return parts._replace(path='/', query='', fragment='').geturl()

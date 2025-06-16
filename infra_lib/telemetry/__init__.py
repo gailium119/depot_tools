868c033640cff559e163a64e591fb295cb49b6a2
Struan Shrimpton <sshrimp@google.com>
2025-06-16 15:38:46 -0700

    Revert "Telemetry: disable for non-linux"
    This reverts commit dcbf502f02ffa11a74e8f33cb7512c2cfab4a65b.
    
    Reason for revert: Fixes have landed
    
    Bug: 326277821
    Original change's description:
    > Telemetry: disable for non-linux
    >
    > Something about collecting traces before explicit opt-in is causing
    > the output on win to get consumed. Disable for non-linux until I can
    > more thoroughly test and figure out what's going on
    >
    > Bug: 326277821
    > Change-Id: I8536cb61078ef98e37c5ab3e1c8abe2141a50eef
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/6648247
    > Auto-Submit: Struan Shrimpton <sshrimp@google.com>
    > Commit-Queue: Struan Shrimpton <sshrimp@google.com>
    > Reviewed-by: Ben Pastene <bpastene@chromium.org>
    
    Bug: 326277821
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I0d122895a832130fef5eebaf4cd4b5896dba4806

--- infra_lib/telemetry/__init__.py
+++ infra_lib/telemetry/__init__.py
@@ -66,8 +66,8 @@ def is_google_host() -> bool:
 def initialize(service_name,
                notice=DEFAULT_BANNER,
                cfg_file=config.DEFAULT_CONFIG_FILE):
-    # TODO(326277821): Renable after more thorough testing
-    if not sys.platform.startswith('linux'):
+    # TODO(326277821): Add support for mac
+    if sys.platform == 'darwin':
         return
 
     cfg = config.Config(cfg_file)
